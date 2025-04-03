a9b6f26eb87a7b65449d2c76652fe015519a0ceb
Steve (Son Nguyen) <darklord.nlqs@gmail.com>
2025-04-03 23:14:30 +0700

    use Hash router for gh pages

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import "../index.css";
 import { AppProvider } from './context/AppContext';
+import { HashRouter } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
-    <AppProvider>
-      <App />
-    </AppProvider>
+    <HashRouter>
+      <AppProvider>
+        <App />
+      </AppProvider>
+    </HashRouter>
   </React.StrictMode>
 ); 
